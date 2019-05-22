afe9d3d796daf6f662dac48ce68614d469b72093
Christian Aichner <contact@aichner-christian.com>
2019-05-22 14:38:46 +0200

    add easy form validation

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 import FacebookLogin from 'react-facebook-login';
 // Icons
-import { FaFacebook } from 'react-icons/fa';
+// import { FaFacebook } from 'react-icons/fa';
 // CSS
 import "./login.scss";
 
@@ -15,10 +15,7 @@ class LoginPage extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   
- 
   responseFacebook = (response) => {
-    console.log(response);
-
     let name = response.name;
     let first_name = response.first_name;
     let last_name = response.last_name;
@@ -30,7 +27,10 @@ class LoginPage extends Component {
       name, first_name, last_name, email, accessToken, picture
     }
     console.log(facebook_data);
-    // Save all data to DB and continue
+
+    // Check if user exists
+    // If not: Save all data to DB and continue to registration process
+    // If yes: Proceed to KIS user area
   }
 
   handleChange(event) {
@@ -43,20 +43,26 @@ class LoginPage extends Component {
   handleSubmit(event) {
     console.log(this.state);
     event.preventDefault();
+    event.target.className += " was-validated";
   }
+
 
   render() {
     return (
       <MDBContainer className="mt-5">
         <MDBRow>
           <MDBCol md="6" className="m-auto text-center">
-            <form>
+            <form
+              className="needs-validation"
+              onSubmit={this.handleSubmit}
+              noValidate
+            >
               <p className="h4 text-center mb-4">Login</p>
               <div className="oAuth">
                 <FacebookLogin
                   appId="2526621870704174"
                   autoLoad={false}
-                  icon={<FaFacebook/>}
+                  //icon={<FaFacebook/>}
                   cssClass="btn-facebook kep-login-facebook kep-login-facebook-medium"
                   fields="name,first_name,last_name,email,picture"
                   textButton="Weiter mit Facebook"
@@ -66,37 +72,49 @@ class LoginPage extends Component {
                   <div className="splitter my-4"><span className="or"><span className="or-text">oder</span></span></div>
               </div>
               <div className="text-left">
-                <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
-                  E-Mail
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="defaultFormLoginEmailEx"
-                  className="form-control"
-                  onChange={ this.handleChange }
-                  value={ this.state.email }
-                />
+                <div>
+                  <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                    E-Mail
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="defaultFormLoginEmailEx"
+                    className="form-control"
+                    onChange={ this.handleChange }
+                    value={ this.state.email }
+                    required
+                  />
+                  <div style={{ top: "auto" }} className="invalid-tooltip">
+                    Bitte geben Sie eine gültige E-Mail Adresse ein
+                  </div>
+                </div>
                 <br />
-                <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
-                  Passwort
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="defaultFormLoginPasswordEx"
-                  className="form-control"
-                  onChange={ this.handleChange }
-                  value={ this.state.password }
-                />
+                <div>
+                  <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+                    Passwort
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="defaultFormLoginPasswordEx"
+                    className="form-control"
+                    onChange={ this.handleChange }
+                    value={ this.state.password }
+                    required
+                  />
+                  <div style={{ top: "auto" }} className="invalid-tooltip">
+                    Bitte geben Sie Ihr Passwort ein
+                  </div>
+                </div>
               </div>
               <div className="text-center mt-4">
                 <MDBBtn
-                  color="grey"
+                  type="submit"
+                  color="primary"
                   rounded
-                  type="button"
+                  icon="lock"
                   className="z-depth-1a"
-                  onClick={ this.handleSubmit }
                 >
                   Log in
                 </MDBBtn>
