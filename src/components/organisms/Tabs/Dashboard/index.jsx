aecc0b303822be5f2066f871f5338317f8eb6817
Christian Aichner <contact@aichner-christian.com>
2019-06-12 14:51:03 +0200

    add Dashboard, Data, Orders, Statistics organisms + add Dashboard template

---
+++ src/components/organisms/Tabs/Dashboard/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+// MDB
+// import...
+
+// CSS
+// import...
+
+// const query = ...
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            
+        }
+    }
+}
+
+export default Dashboard;
